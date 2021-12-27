3be0ff3829ad5068dffe502da4389fd8404925fa
Omar Faruk Shakil <omarshakil8@gmail.com>
2021-12-27 16:48:06 +0600

    balanced parenthesis

--- removeEven.js
+++ removeEven.js
@@ -7,3 +7,10 @@ function removeEven(arr) {
   return odds // Return the new list
 }
 console.log(removeEven([3, 2, 41, 3, 34]))
+
+
+
+// function removeEven(arr) {
+//     return arr.filter((v => (v % 2) != 0))
+// }
+// console.log(removeEven([3,2,41,3,34]))
